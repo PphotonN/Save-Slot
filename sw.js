845e1b1c25255e98900e7b0d0a0322d0b5b3ab9b
MK <103195949+PphotonN@users.noreply.github.com>
2026-07-27 14:08:38 +0300

    Refresh PWA cache for startup fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "save-slot-shell-v6";
+const CACHE_NAME = "save-slot-shell-v7";
 const APP_SHELL = ["./", "./index.html", "./styles-v3.css", "./app-v3-core.js", "./app-v3-wikidata.js", "./app-v3-steam.js", "./app-v3-results.js", "./app-v3-manager.js", "./app-v3-init.js", "./app-v4.js", "./app-v5.js", "./manifest.webmanifest", "./assets/icon.svg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim())));
