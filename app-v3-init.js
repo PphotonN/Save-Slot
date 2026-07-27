4631a785815b544b574014caaa1136b4b71adf99
MK <103195949+PphotonN@users.noreply.github.com>
2026-07-27 14:44:56 +0300

    Load platform boxart validation patch

--- app-v3-init.js
+++ app-v3-init.js
@@ -10,9 +10,10 @@
     });
   }
 
-  loadScript("./app-v4.js?v=8")
-    .then(() => loadScript("./app-v5.js?v=8"))
-    .then(() => loadScript("./app-v6.js?v=8"))
+  loadScript("./app-v4.js?v=9")
+    .then(() => loadScript("./app-v5.js?v=9"))
+    .then(() => loadScript("./app-v6.js?v=9"))
+    .then(() => loadScript("./app-v7.js?v=9"))
     .catch(error => {
       console.error(error);
       setSourceState("error", "ПОМИЛКА ЗАПУСКУ");
