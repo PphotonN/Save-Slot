eb7ab99413a902b7f137365910bfb6338a215309
MK <103195949+PphotonN@users.noreply.github.com>
2026-07-27 16:11:49 +0300

    Load Save Slot 0.6 sources and styles

--- app-v3-init.js
+++ app-v3-init.js
@@ -10,12 +10,25 @@
     });
   }
 
-  loadScript("./app-v4.js?v=11")
-    .then(() => loadScript("./app-v5.js?v=11"))
-    .then(() => loadScript("./app-v6.js?v=11"))
-    .then(() => loadScript("./app-v7.js?v=11"))
-    .then(() => loadScript("./app-v8.js?v=11"))
-    .then(() => loadScript("./app-v9.js?v=11"))
+  function loadStyle(src) {
+    return new Promise(resolve => {
+      const link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.href = src;
+      link.onload = resolve;
+      link.onerror = resolve;
+      document.head.append(link);
+    });
+  }
+
+  loadStyle("./styles-v10.css?v=12");
+  loadScript("./app-v4.js?v=12")
+    .then(() => loadScript("./app-v5.js?v=12"))
+    .then(() => loadScript("./app-v6.js?v=12"))
+    .then(() => loadScript("./app-v7.js?v=12"))
+    .then(() => loadScript("./app-v8.js?v=12"))
+    .then(() => loadScript("./app-v9.js?v=12"))
+    .then(() => loadScript("./app-v10.js?v=12"))
     .catch(error => {
       console.error(error);
       setSourceState("error", "ПОМИЛКА ЗАПУСКУ");
