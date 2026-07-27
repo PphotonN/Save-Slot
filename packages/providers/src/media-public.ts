9fcf84096075f5e3628f9d7bb93c45d0c154ccc3
MK <103195949+PphotonN@users.noreply.github.com>
2026-07-27 23:35:32 +0300

    Route public media exports through robust Libretro provider

---
+++ packages/providers/src/media-public.ts
@@ -0,0 +1,11 @@
+export {
+  SteamMediaProvider,
+  mergeEnrichment,
+  type ReleaseEnricher,
+  type ReleaseEnrichment,
+  type SteamMediaProviderOptions,
+} from './media';
+export {
+  RobustLibretroMediaProvider as LibretroMediaProvider,
+  type RobustLibretroMediaProviderOptions as LibretroMediaProviderOptions,
+} from './libretro';
