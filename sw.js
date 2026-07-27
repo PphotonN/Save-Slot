73979078e3d154b47e045beed1c885961ad4fafa
MK <103195949+PphotonN@users.noreply.github.com>
2026-07-27 14:42:14 +0300

    Cache Save Slot 0.5 shell

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "save-slot-shell-v7";
-const APP_SHELL = ["./", "./index.html", "./styles-v3.css", "./app-v3-core.js", "./app-v3-wikidata.js", "./app-v3-steam.js", "./app-v3-results.js", "./app-v3-manager.js", "./app-v3-init.js", "./app-v4.js", "./app-v5.js", "./manifest.webmanifest", "./assets/icon.svg"];
+const CACHE_NAME = "save-slot-shell-v8";
+const APP_SHELL = ["./", "./index.html", "./styles-v3.css", "./styles-v6.css", "./app-v3-core.js", "./app-v3-wikidata.js", "./app-v3-steam.js", "./app-v3-results.js", "./app-v3-manager.js", "./app-v3-init.js", "./app-v4.js", "./app-v5.js", "./app-v6.js", "./manifest.webmanifest", "./assets/icon.svg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => {
