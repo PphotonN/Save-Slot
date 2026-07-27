874f902b4373e6619c05c65b4e2fb02d63eb5447
MK <103195949+PphotonN@users.noreply.github.com>
2026-07-27 15:45:26 +0300

    Load stable reveal ordering update

--- app-v3-init.js
+++ app-v3-init.js
@@ -10,11 +10,12 @@
     });
   }
 
-  loadScript("./app-v4.js?v=10")
-    .then(() => loadScript("./app-v5.js?v=10"))
-    .then(() => loadScript("./app-v6.js?v=10"))
-    .then(() => loadScript("./app-v7.js?v=10"))
-    .then(() => loadScript("./app-v8.js?v=10"))
+  loadScript("./app-v4.js?v=11")
+    .then(() => loadScript("./app-v5.js?v=11"))
+    .then(() => loadScript("./app-v6.js?v=11"))
+    .then(() => loadScript("./app-v7.js?v=11"))
+    .then(() => loadScript("./app-v8.js?v=11"))
+    .then(() => loadScript("./app-v9.js?v=11"))
     .catch(error => {
       console.error(error);
       setSourceState("error", "ПОМИЛКА ЗАПУСКУ");
