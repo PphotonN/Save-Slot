37607239b83694beb82b35ea23ce8abeebe1cde3
MK <103195949+PphotonN@users.noreply.github.com>
2026-07-27 16:34:35 +0300

    Refresh cache for rendering fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "save-slot-shell-v13";
-const APP_SHELL = ["./", "./index.html", "./styles-v3.css", "./styles-v6.css", "./styles-v8.css", "./styles-v10.css", "./app-v3-core.js", "./app-v3-wikidata.js", "./app-v3-steam.js", "./app-v3-results.js", "./app-v3-manager.js", "./app-v3-init.js", "./app-v4.js", "./app-v5.js", "./app-v6.js", "./app-v7.js", "./app-v8.js", "./app-v9.js", "./app-v10.js", "./app-v11.js", "./manifest.webmanifest", "./assets/icon.svg"];
+const CACHE_NAME = "save-slot-shell-v14";
+const APP_SHELL = ["./", "./index.html", "./styles-v3.css", "./styles-v6.css", "./styles-v8.css", "./styles-v10.css", "./app-v3-core.js", "./app-v3-wikidata.js", "./app-v3-steam.js", "./app-v3-results.js", "./app-v3-manager.js", "./app-v3-init.js", "./app-v4.js", "./app-v5.js", "./app-v6.js", "./app-v7.js", "./app-v8.js", "./app-v9.js", "./app-v10.js", "./app-v11.js", "./app-v12.js", "./manifest.webmanifest", "./assets/icon.svg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => {
