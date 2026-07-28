00d85a34631fd6cb0dd41c58e756e27dcfc33ab4
MK <103195949+PphotonN@users.noreply.github.com>
2026-07-29 01:29:05 +0300

    Load Android online-cache engine

--- app-v3-init.js
+++ app-v3-init.js
@@ -21,16 +21,17 @@
     });
   }
 
-  loadStyle("./styles-v10.css?v=14");
-  loadScript("./app-v4.js?v=14")
-    .then(() => loadScript("./app-v5.js?v=14"))
-    .then(() => loadScript("./app-v6.js?v=14"))
-    .then(() => loadScript("./app-v7.js?v=14"))
-    .then(() => loadScript("./app-v8.js?v=14"))
-    .then(() => loadScript("./app-v9.js?v=14"))
-    .then(() => loadScript("./app-v10.js?v=14"))
-    .then(() => loadScript("./app-v11.js?v=14"))
-    .then(() => loadScript("./app-v12.js?v=14"))
+  loadStyle("./styles-v10.css?v=15");
+  loadScript("./app-v4.js?v=15")
+    .then(() => loadScript("./app-v5.js?v=15"))
+    .then(() => loadScript("./app-v6.js?v=15"))
+    .then(() => loadScript("./app-v7.js?v=15"))
+    .then(() => loadScript("./app-v8.js?v=15"))
+    .then(() => loadScript("./app-v9.js?v=15"))
+    .then(() => loadScript("./app-v10.js?v=15"))
+    .then(() => loadScript("./app-v11.js?v=15"))
+    .then(() => loadScript("./app-v12.js?v=15"))
+    .then(() => loadScript("./app-v13.js?v=15"))
     .catch(error => {
       console.error(error);
       setSourceState("error", "ПОМИЛКА ЗАПУСКУ");
